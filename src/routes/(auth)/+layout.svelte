3ad78f7c8f151fe08a92562a4a27e307aa357702
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-09-16 21:51:12 -0300

    feat: login

---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,7 @@
+<div class="bg-slate-100 flex flex-col items-center justify-center h-screen w-screen">
+	<h2 class="text-5xl -mt-20 font-black tracking-tight mb-16 text-indigo-700">Entreprenuer</h2>
+
+	<div class="pt-12 pb-16 px-14 bg-white shadow-2xl">
+		<slot />
+	</div>
+</div>
---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+
+	export let form: ActionData;
+</script>
+
+<div class="container mx-auto">
+	<form method="post" use:enhance>
+		<div class="mb-4">
+			<label for="username">Username</label>
+			<input
+				type="text"
+				id="username"
+				name="username"
+				class="border w-full py-3 px-4 mt-2 rounded-md"
+			/>
+
+			{#if form?.error}
+				<span class="text-red-500">{form?.message}</span>
+			{/if}
+		</div>
+
+		<div class="mb-8">
+			<label for="password">Password</label>
+			<input
+				type="password"
+				id="password"
+				name="password"
+				class="border w-full py-3 px-4 mt-2 rounded-md"
+			/>
+		</div>
+
+		<button
+			type="submit"
+			class="p-4 w-full text-white bg-indigo-600 rounded-xl text-lg font-semibold">Login</button
+		>
+	</form>
+</div>
