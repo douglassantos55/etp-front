8697e8b26fcb8d382ae22430d92bc2b15bea7438
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-09-16 21:54:25 -0300

    refactor: layout

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { user } from '$lib/stores/user';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+
+	$: console.log(data);
+	$: user.set(data.user);
+</script>
+
+<header class="py-4 bg-stone-100 border-b border-stone-200">
+	<div class="container mx-auto">
+		<div class="inline-flex gap-2.5">
+			<a href="/company">
+				<img src={$user.company_logo} alt={$user.company_name} class="w-14 h-full object-cover" />
+			</a>
+
+			<div class="py-1">
+				<h2 class="text-xl font-semibold tracking-tight">{$user.company_name}</h2>
+				<span class="text-medium tracking-wide">$ {$user.available_cash}</span>
+			</div>
+		</div>
+	</div>
+</header>
+
+<slot />
--- src/routes/+layout.ts
+++
@@ -1,6 +0,0 @@
-import type { LayoutServerLoad } from "./$types";
-
-export const load: LayoutServerLoad = async function({ url, fetch }) {
-    const response = await fetch(`http://localhost:3000/users/${url.searchParams.get('id')}`);
-    return { user: await response.json() };
-}
