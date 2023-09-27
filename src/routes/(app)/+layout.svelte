938ad3bda12d84de70d3afb810636cbe17b80cdc
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-09-27 14:19:45 -0300

    refactor: menu

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -10,24 +10,27 @@
 
 <header class="py-4 bg-stone-100 border-b border-stone-200">
 	<div class="container mx-auto">
-		<div class="inline-flex gap-2.5">
-			<a href="/company">
-				<img src={$user.company_logo} alt={$user.company_name} class="w-14 h-full object-cover" />
-			</a>
+		<div class="flex items-center">
+			<div class="inline-flex gap-2.5">
+				<a href="/company">
+					<img src={$user.company_logo} alt={$user.company_name} class="w-14 h-full object-cover" />
+				</a>
 
-			<div class="py-1">
-				<h2 class="text-xl font-semibold tracking-tight">{$user.company_name}</h2>
-				<span class="text-medium tracking-wide">$ {$user.available_cash}</span>
+				<div class="py-1">
+					<h2 class="text-xl font-semibold tracking-tight">{$user.company_name}</h2>
+					<span class="text-medium tracking-wide">$ {$user.available_cash}</span>
+				</div>
+			</div>
+
+			<div class="ml-auto">
+				<a href="/inventory" class="px-4">Inventory</a>
+				<a href="/market" class="px-4">Market</a>
+				<a href="/financing" class="px-4">Finances</a>
+				<a href="/research" class="px-4">Research</a>
+				<a href="/chat" class="px-4">Chat</a>
 			</div>
 		</div>
 	</div>
 </header>
 
 <slot />
-
-<div class="flex absolute bottom-10 justify-center w-full gap-10">
-	<a href="/inventory" class="w-24 h-24 border inline-flex items-center justify-center rounded-full">Inventory</a>
-	<a href="/market" class="w-24 h-24 border inline-flex items-center justify-center rounded-full">Market</a>
-	<a href="/financing" class="w-24 h-24 border inline-flex items-center justify-center rounded-full">Finances</a>
-	<a href="/research" class="w-24 h-24 border inline-flex items-center justify-center rounded-full">Research</a>
-</div>
