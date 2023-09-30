f5fb7913c5f542bd03ddba39a257f0aed7845043
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-09-30 16:45:57 -0300

    feat: chat

---
+++ src/routes/(app)/chat/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	const companies = [
+		{ name: 'Marfrig', last_message: 'Hey man, that is pretty cool!', timestamp: 'yesterday' },
+		{ name: 'Coca-Cola', last_message: 'That sounds good, thanks', timestamp: '2 days ago' },
+		{
+			name: 'Tesla',
+			last_message: 'You cannot miss this oportunity',
+			timestamp: 'a few seconds ago'
+		},
+		{
+			name: 'Microsoft',
+			last_message: 'No problem, we will look into it',
+			timestamp: 'one minute ago'
+		},
+		{ name: 'Amazon', last_message: 'Your order is on the way!', timestamp: '30 minutes ago' },
+		{ name: 'Turso', last_message: 'Check this out', timestamp: '2 hours ago' },
+		{ name: 'Google', last_message: 'Hey man, that is pretty cool!', timestamp: 'yesterday' },
+		{ name: 'Casas Bahia', last_message: 'Delivery for you', timestamp: 'a year ago' },
+		{
+			name: 'Netflix',
+			last_message: 'New shows are available in your region',
+			timestamp: 'a month ago'
+		},
+		{ name: 'McDonalds', last_message: '', timestamp: '' }
+	];
+</script>
+
+<div class="container px-4 mx-auto py-12">
+	<div class="grid sm:grid-cols-2 py-2 gap-8">
+		{#each { length: 10 } as _, i}
+			<a href={`/chat/${i}`} class="flex items-center gap-4">
+				<img
+					src="https://picsum.photos/100"
+					class="w-16 h-16 mb-1 rounded-full object-cover"
+					alt="company"
+				/>
+
+				<div class="flex-shrink">
+					<h3 class="font-semibold">{companies[i].name}</h3>
+
+					<p>{companies[i].last_message}</p>
+					<p class="text-xs text-gray-400">{companies[i].timestamp}</p>
+				</div>
+			</a>
+		{/each}
+	</div>
+</div>
---
+++ src/routes/(app)/chat/[id]/+page.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+	function scroll() {
+		setTimeout(() => window.scrollTo(0, document.body.scrollHeight), 2);
+	}
+</script>
+
+<div class="container px-4 py-12 mx-auto">
+	<div class="flex items-center py-2 gap-3 sticky top-0 bg-white">
+		<img
+			src="https://picsum.photos/100"
+			alt="Casas Bahia"
+			class="w-12 h-12 rounded-full object-cover"
+		/>
+		<div>
+			<h2 class="font-semibold text-xl leading-4">Casas Bahia</h2>
+			<span class="text-sm text-slate-400">Typing...</span>
+		</div>
+
+		<a href="/chat" class="ml-auto">&larr; Back</a>
+	</div>
+
+	<div class="flex flex-col flex-grow">
+		<div class="pt-6 pb-10 space-y-3 overflow-y-auto" use:scroll>
+			<div>
+				<p class="inline-block bg-gray-200 py-2 px-4 rounded-full">Some message</p>
+				<p class="text-xs text-gray-400 ms-3">a few seconds ago</p>
+			</div>
+
+			<div class="text-right">
+				<p class="inline-block bg-indigo-100 py-2 px-4 rounded-full">Some reply</p>
+				<p class="text-xs text-gray-400 mr-3">a few seconds ago</p>
+			</div>
+
+			<div class="text-right">
+				<p class="inline-block bg-indigo-100 py-2 px-4 rounded-full">Some other reply</p>
+				<p class="text-xs text-gray-400 mr-3">a few seconds ago</p>
+			</div>
+
+			<div>
+				<p class="inline-block bg-gray-200 py-2 px-4 rounded-full">Some message</p>
+				<p class="text-xs text-gray-400 ms-3">a few seconds ago</p>
+			</div>
+
+			<div>
+				<p class="inline-block bg-gray-200 py-2 px-4 rounded-full">Some message</p>
+				<p class="text-xs text-gray-400 ms-3">a few seconds ago</p>
+			</div>
+
+			<div>
+				<p class="inline-block bg-gray-200 py-2 px-4 rounded-full">Some message</p>
+				<p class="text-xs text-gray-400 ms-3">a few seconds ago</p>
+			</div>
+
+			<div class="text-right">
+				<p class="inline-block bg-indigo-100 py-2 px-4 rounded-full">Some reply</p>
+				<p class="text-xs text-gray-400 mr-3">a few seconds ago</p>
+			</div>
+
+			<div class="text-right">
+				<p class="inline-block bg-indigo-100 py-2 px-4 rounded-full">Some other reply</p>
+				<p class="text-xs text-gray-400 mr-3">a few seconds ago</p>
+			</div>
+
+			<div class="text-right">
+				<p class="inline-block bg-indigo-100 py-2 px-4 rounded-full">Some reply</p>
+				<p class="text-xs text-gray-400 mr-3">a few seconds ago</p>
+			</div>
+
+			<div class="text-right">
+				<p class="inline-block bg-indigo-100 py-2 px-4 rounded-full">Some other reply</p>
+				<p class="text-xs text-gray-400 mr-3">a few seconds ago</p>
+			</div>
+
+			<div>
+				<p class="inline-block bg-gray-200 py-2 px-4 rounded-full">Some message</p>
+				<p class="text-xs text-gray-400 ms-3">a few seconds ago</p>
+			</div>
+
+			<div>
+				<p class="inline-block bg-gray-200 py-2 px-4 rounded-full">Some message</p>
+				<p class="text-xs text-gray-400 ms-3">a few seconds ago</p>
+			</div>
+
+			<div class="text-right">
+				<p class="inline-block bg-blue-100 py-2 px-4 rounded-full">Some other reply</p>
+				<p class="text-xs text-gray-400 mr-3">a few seconds ago</p>
+			</div>
+
+			<div>
+				<p class="inline-block bg-gray-200 py-2 px-4 rounded-full">Some message</p>
+				<p class="text-xs text-gray-400 ms-3">a few seconds ago</p>
+			</div>
+		</div>
+	</div>
+
+	<form>
+		<input
+			autofocus
+			name="message"
+			placeholder="Enter your message"
+			class="fixed bottom-0 left-0 w-full border-t px-4 py-2"
+		/>
+	</form>
+</div>
